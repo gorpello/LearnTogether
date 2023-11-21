2fc1a463a6958e618da07f0b8e68cd23aae82157
Gianluca Orpello <g.orpello@gmail.com>
2023-11-21 12:46:38 +0100

    Adding an xcode project becouse why not?

---
+++ LearnTogether/LearnTogether/LearnTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTogetherApp.swift
+//  LearnTogether
+//
+//  Created by Gianluca Orpello on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
